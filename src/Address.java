9bda6f225c646b1398d5999869554889f63cce44
Conrodot <connor.emery@yahoo.com>
2017-03-10 09:07:45 -0500

    Update Address.java

--- src/Address.java
+++ src/Address.java
@@ -1,3 +1,5 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Address
 {
@@ -7,13 +9,49 @@ public class Address
     private String postalCode;
 
     public Address (String street, String city, String postalCode) throws Exception
-    { };
+    {
+    	this.street = street;
+    	this.city = city;
+    	this.postalCode = postalCode;
+    	this.unit = 0;
+    	if (!this.isValidPostalCode(this.postalCode)){
+    		throw new Exception();
+    	}
+    };
     public Address (int unit, String street, String city, String postalCode) throws Exception
-    { };
+    { 
+    	this.street = street;
+    	this.city = city;
+    	this.postalCode = postalCode;
+    	this.unit = unit;
+    	if (!this.isValidPostalCode(this.postalCode)){
+    		throw new Exception();
+    	}
+    };
 
-     public String toString() { return null;}
-     public boolean equals(Object o) { return false; }
+    public String toString() { 
+    	String a = "Unit: " + this.unit + ", Street: " + this.street + ", City: " + this.city  + ", Postal Code: " + this.postalCode;
+    	return a;
+    	}
+    
+    public boolean equals(Object o) { 
+    	Address a = (Address) o;
+    	if (!this.street.equals(a.street))
+    		return false;
+    	else if (!this.city.equals(a.city))
+    		return false;
+    	else if (!this.postalCode.equals(a.postalCode))
+    		return false;
+    	else if (! (this.unit == a.unit))
+    		return false;
+    	return true; 
+    	}
      
-     private boolean isValidPostalCode( String postalCode ) { return false; }
+    private boolean isValidPostalCode( String postalCode ) { 
+    	String regex = "[A-z][0-9][A-z] ?[0-9][A-z][0-9]$";
+    	Pattern pattern = Pattern.compile(regex);
+    	Matcher matcher = pattern.matcher(postalCode);
+    	return matcher.matches();
+    	}
  
 } 
