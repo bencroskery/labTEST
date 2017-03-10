37335b01d49f91c645a789b6856cf884a09d1884
Dillon Verhaeghe <dillonverhaeghe@LABS.SCE.CARLETON.CA>
2017-03-10 09:09:39 -0500

    address testing

---
+++ src/testingAddress.java
@@ -0,0 +1,79 @@
+package unitTesting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testingAddress {
+
+	@Test
+	public void testAddress1() {
+		try {
+			Address testAddress = new Address("thisSucks", "Ottawa","k1g5u6");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("Exception");
+		}
+		String expection = "Unit: 0, Street: thisSucks, City: Ottawa, Postal Code: k1g5u6";
+		
+		if (!(testAddress.toString.equals(expection)) ){
+			fail("Address was not created with the first Construtor");
+		}
+	}
+	
+	@Test
+	public void testAddress2() {
+		try {
+			Address testAddress = new Address(10, "thisSucks", "Ottawa","k1g5u6");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("Exception");
+		}
+		String expection = "Unit: 10, Street: thisSucks, City: Ottawa, Postal Code: k1g5u6";
+		
+		if (!(testAddress.toString.equals(expection)) ){
+			fail("Address was not created with the second Construtor");
+		}
+	}
+	
+	@Test
+	public void testAddress3() {
+		try {
+			Address testAddress = new Address(10, "thisSucks", "Ottawa","1kg5u6");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			return;
+		}
+		fail("Did not get exception for invaild postal code");
+	}
+	
+	public void testEquals1() {
+		try {
+			Address testAddress = new Address(10, "thisSucks", "Ottawa","k1g5u6");
+			Address testAddress1 = new Address(10, "thisSucks", "Ottawa","k1g5u6");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("Exception");
+		}
+		if(!(testAddress.equals(testAddress1))) {
+			fail("Equals did not work");
+		}	
+	}
+	
+	public void testEquals2() {
+		try {
+			Address testAddress = new Address(10, "thisisPoop", "Ottawa","k1g5u6");
+			Address testAddress1 = new Address(10, "thisSucks", "Ottawa","k1g5u6");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("Exception");
+		}
+		if(!(testAddress.equals(testAddress1))) {
+			fail("Equals did not work");
+		}	
+	}
+}
