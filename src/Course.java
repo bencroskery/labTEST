135497cd8fcd81989f418d6df35f9352133aafd0
Conrodot <connor.emery@yahoo.com>
2017-03-10 09:29:23 -0500

    Update Course.java

--- src/Course.java
+++ src/Course.java
@@ -7,20 +7,38 @@ public class Course
     private Professor professor;
     private ArrayList<Student>students;
     
-    public Course( String id, String name) {};
+    public Course( String id, String name) {
+    	this.id = id;
+    	this.name = name;
+    	this.professor = null;
+    	this.students = null;
+    };
     
     public String getId() { return id; }
-    public String getName( ) { return null; }
+    public String getName( ) { return name; }
 
     public String toString() { return null; }
-    public boolean equals(Object o) { return false; }
+    public boolean equals(Object o) { 
+    	Course c = (Course) o;
+    	if (!this.id.equals(c.id))
+    		return false;
+    	else if (!this.name.equals(c.name))
+    		return false;
+    	else if (!this.professor.equals(c.professor))
+    		return false;
+    	else if (!this.students.equals(c.students))
+    		return false;
+    	return true;
+    	}
 
-    public void setProfessor(Professor professor) {};
-    public Professor getProfessor() { return null; }
+    public void setProfessor(Professor professor) {
+    	this.professor = professor;
+    };
+    public Professor getProfessor() { return this.professor; }
 
-    public void addStudent( Student student ) {};
-    public void removeStudent( Student student ) {};
-    public Student[] getStudents() { return null; }
+    public void addStudent( Student student ) {students.add(student);};
+    public void removeStudent( Student student ) {students.remove(student);};
+    public ArrayList<Student> getStudents() { return this.students; }
     
     
 }
