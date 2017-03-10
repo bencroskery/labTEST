615436e062a216f47657f2e3b1206cb72ac3ddf7
virajdave <virajdave@cmail.carleton.ca>
2017-03-10 10:43:04 -0500

    Missing 2 methods

--- src/TestingPerson.java
+++ src/TestingPerson.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 public class TestingPerson {
 
 	@Test
+	
+	
+	@Test
 	public void testToString(){
 		Person test = new Person ("Billy", "Bob");
 		String result = test.toString();
@@ -34,7 +37,7 @@ public class TestingPerson {
 	@Test
 	public void testGetFirstName(){
 		Person test4 = new Person ("Tommy", "Wang");
-		String result3 = test4.getFirstName()
+		String result3 = test4.getFirstName();
 		if(!(result3.equals("Tommy"))){
 			fail("Getter for the first name didn't work");
 		}
@@ -43,6 +46,54 @@ public class TestingPerson {
 	@Test
 	public void testGetFullName(){
 		Person test5 = new Person("Cam", "Birdi");
-		if(!(test5.getFullName()= "First Name: Cam, Last Name: Birdi")));
+		String result4 = test5.getFullName();
+		if(!(result4.equals(test5.toString()))){
+			fail("toString fails horribly");
+		};
 	}
+	
+	@Test
+	public void testSetLastName(){
+		Person test7 = new Person ("Rico", "Hola");
+		test7.setLastName("Smith");
+		if(!(test7.getLastName().equals("Smith"))){
+			fail("Setter for Last Name");
+		}
+	}
+	
+	@Test 
+	public void testSetAddress() {
+		Person test6 = new Person ("Davey", "Boy");
+		try {
+			Address add1 = new Address("42 Fifth Avenue", "New York", "T4E 5F8");
+			test6.setAddress(add1);
+			if(!(test6.getAddress().equals(add1))){
+				fail("setter didn't work");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testgetAddress(){
+		Person test8 = new Person ("Ron", "Cherry");
+		try {
+			Address add2 = new Address("75 Smyth Road", "Ottawa", "R4E 3R4");
+			test8.setAddress(add2);
+			Address add3 = test8.getAddress();
+			if(!(add3.equals(add2))){
+				fail("Getter for the address didn't work");
+			}
+		}
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	
+	@Test
+	public void testGetCourses()
+	
 }
