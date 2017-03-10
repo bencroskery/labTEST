83d566380586067dcebb823e77aa9ab0156c1b46
dillonverhaeghe <dillonverhaeghe@mc6045-07.labs.sce.carleton.ca>
2017-03-10 10:29:11 -0500

    hellllllpppppppppppppppp meeeeeeeeeeeeeeeeeeeeeeeeeee

--- src/testingProfessor.java
+++ src/testingProfessor.java
@@ -14,11 +14,20 @@ public class testingProfessor {
 	}
 	
 	@Test
-	public void testSetOffice() {
+	public void testSetOffice1() {
 		Professor prof = new Professor("I","Stink");
-		prof.
-		if (!(profString.toString().equals("First Name: I, Last Name: Stink"))) {
-			fail("Professor contructor has failed or to string");
+		prof.setOffice("Court");
+		if (!(prof.getOffice().equals("Court"))) {
+			fail("Failed setting office got: " + prof.getOffice());
+		}
+	}
+	
+	@Test
+	public void testSetOffice2() {
+		Professor prof = new Professor("I","Stink");
+		prof.setOffice("Court");
+		if (!(prof.getOffice().equals("hahhahahahah"))) {
+			fail("Failed setting office got: " + prof.getOffice());
 		}
 	}
 
